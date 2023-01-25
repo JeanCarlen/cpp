b4674e8c9a7e44aae860ea96f038c17d980c5842
JeanCarlen <94684234+JeanCarlen@users.noreply.github.com>
2023-01-25 16:38:18 +0100

    start converter

--- CPP_06/ex00/Converter.cpp
+++ CPP_06/ex00/Converter.cpp
@@ -6,8 +6,33 @@
 /*   By: jcarlen <jcarlen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:03:01 by jcarlen           #+#    #+#             */
-/*   Updated: 2023/01/25 16:03:41 by jcarlen          ###   ########.ch       */
+/*   Updated: 2023/01/25 16:37:48 by jcarlen          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Converter.hpp"
+
+Converter::Converter()
+{
+
+}
+
+Converter::~Converter()
+{
+	
+}
+
+Converter::Converter(Converter &og)
+{
+	
+}
+
+Converter::Converter &operator=(Converter const &rhs)
+{
+	
+}
+
+void Converter::Convert()
+{
+	
+}
--- CPP_06/ex00/Converter.hpp
+++ CPP_06/ex00/Converter.hpp
@@ -6,7 +6,7 @@
 /*   By: jcarlen <jcarlen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:02:57 by jcarlen           #+#    #+#             */
-/*   Updated: 2023/01/25 16:17:31 by jcarlen          ###   ########.ch       */
+/*   Updated: 2023/01/25 16:37:49 by jcarlen          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,20 @@
 class Converter
 {
 	private:
-			char	letter;
-			int		num;
-			float	flt;
-			double	dbl;
+			int		Cint();
+			char 	Cchar();
+			float	Cfloat();
+			double 	Cdouble();
 
 	public:
 			Converter();
 			~Converter();
+			Converter(const std::string& value);
 			Converter(Converter &og);
 			Converter	&operator=(Converter const &rhs);
 
-			int	Cint(int num);
-			int Cchar(char letter);
-			int	Cfloat(float flt);
-			int Cdouble(double dbl);
+			void Convert();
+
 };
 
 std::ostream& operator<<( std::ostream& out, const Converter& rhs );
